92b26996224d289602dc63e41243bfbc2a5b3b93
charukiewicz <c.charukiewicz@gmail.com>
2015-05-16 11:48:02 -0500

    Added Inconsolata custom font

--- site.hs
+++ site.hs
@@ -11,6 +11,10 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "files/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
