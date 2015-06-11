92dca9068966a14ac4c47f8c9a4cbb7d6d897ffa
charukiewicz <c.charukiewicz@gmail.com>
2015-06-10 22:23:42 -0500

    modified site.hs to produce paths without the .html extension

--- site.hs
+++ site.hs
@@ -2,9 +2,50 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
+import           Data.List (isInfixOf)
+import           System.FilePath.Posix (splitFileName,takeBaseName
+                                        ,takeDirectory, (</>), replaceDirectory)
 
 
 --------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+
+-- The functions in this section were borrowed from the Main.hs file of
+-- https://github.com/mdgriffith/mechanical-elephant.
+
+-- replace a foo/bar.md by foo/bar/index.html
+-- this way the url looks like: foo/bar in most browsers
+niceRoute :: Routes
+niceRoute = customRoute createIndexRoute
+  where
+    createIndexRoute ident =
+        takeDirectory (toFilePath ident) </> takeBaseName (toFilePath ident) </> "index.html"
+
+baseRoute :: Routes
+baseRoute = customRoute base
+  where
+    base ident = replaceDirectory (toFilePath ident) ""
+
+
+niceBaseRoute :: Routes
+niceBaseRoute = customRoute base
+    where 
+      base ident = takeBaseName (toFilePath ident) </> "index.html"
+
+-- replace url of the form foo/bar/index.html by foo/bar
+removeIndexHtml :: Item String -> Compiler (Item String)
+removeIndexHtml item = return $ fmap (withUrls removeIndexStr) item
+  where
+    removeIndexStr :: String -> String
+    removeIndexStr url = case splitFileName url of
+        (dir, "index.html") | isLocal dir -> dir
+        _                                 -> url
+    isLocal :: [Char] -> Bool
+    isLocal uri = not (isInfixOf "://" uri)
+
+--------------------------------------------------------------------------------
+
 main :: IO ()
 main = hakyll $ do
     match "images/*" $ do
@@ -24,17 +65,21 @@ main = hakyll $ do
         compile compressCssCompiler
 
     match (fromList ["about.md", "resume.md"]) $ do
-        route   $ setExtension "html"
+        --route   $ setExtension "html"
+        route $ niceBaseRoute
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
+            >>= removeIndexHtml
 
     match "posts/*" $ do
-        route $ setExtension "html"
+        --route $ setExtension "html"
+        route $ niceRoute
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
+            >>= removeIndexHtml
 
 {-
     create ["archive.html"] $ do
@@ -64,6 +109,7 @@ main = hakyll $ do
                 >>= applyAsTemplate indexCtx
                 >>= loadAndApplyTemplate "templates/default.html" indexCtx
                 >>= relativizeUrls
+                >>= removeIndexHtml
 
     match "templates/*" $ compile templateCompiler
 
